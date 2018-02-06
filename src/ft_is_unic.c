16fd33892cf31f7de697110cfa04a7c34f6be253
Martin SICOT <msicot@e1r4p13.42.fr>
2018-02-06 17:03:18 +0100

    hw

--- src/ft_is_unic.c
+++ src/ft_is_unic.c
@@ -6,7 +6,7 @@
 /*   By: msicot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 14:09:26 by msicot            #+#    #+#             */
-/*   Updated: 2018/02/06 14:22:42 by msicot           ###   ########.fr       */
+/*   Updated: 2018/02/06 16:51:41 by msicot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 char	*ft_is_unic(va_list ap, t_arg *l)
 {
 	char	*s;
+	wchar_t	u;
 	
-	int i = l->width;
-	s = "troll";
-	if (ap)
-		l->width = i;
-	printf("size =%lu\n", sizeof(wchar_t));
+	l->preci = (l->point == 1)? l->preci : 0;
+	u = va_arg(ap, wchar_t);	
+	s = NULL;
 	return (s);
 }
