be1b025320a08f4a15118419af7174c9ce40ce0a
Martin SICOT <msicot@e1r7p1.42.fr>
2018-02-14 18:19:03 +0100

    main

--- main.c
+++ main.c
@@ -2,94 +2,137 @@
 #include <stdio.h>
 #include <wchar.h>
 
-#define _TEST_ "T0->%05.2p<-", 0 
-#define _TEST1_ "T1->%5.0p<-", 0
-#define _TEST2_ "T2->%05p<-", 0
-#define _TEST3_ "T3 >------------<%-#40.36zx>------------<", -1373977486
-#define _TEST4_ "T4 ->"
+#define _TEST_ "@moulitest: %.5u", 42
+#define _TEST1_ "hello %"
+#define _TEST2_ "%yyy"
+#define _TEST3_ "% %"
+#define _TEST4_ "T4 ->%p", 0
 #define _TEST5_ "hello "
 
 int main()
 {
-	char *c = "yo";
-	wchar_t	s[4];
 char* l = setlocale(LC_ALL, "");
-	s[0] = 0x53;
-	s[1] = 0x3abc;
-	s[2] = 0x81000;
-	s[3] = '\0';
- //while(1)
-	//	i = 1;
-	//
-	 //printf(_TEST_);ft_putchar('\n');
-
-//	 ft_printf(_TEST_); ft_putchar('\n');
-//	 ft_putchar('\n');
-	printf(" %d\n", printf(_TEST_));
-	printf(" %d\n\n",ft_printf(_TEST_));
-	
-
-	printf(" %d\n", printf(_TEST1_));
-	printf(" %d\n\n",ft_printf(_TEST1_));
-	printf(" %d\n", printf(_TEST2_));
-	printf(" %d\n\n",ft_printf(_TEST2_));
-
-	printf(" %d\n", printf(_TEST3_));
-	printf(" %d\n\n",ft_printf(_TEST3_));
-	printf(" %d\n", printf(_TEST4_));
-	printf(" %d\n",ft_printf(_TEST4_));
+// printf("%C\n", (char)0);
+//	ft_printf("%s\n", NULL);
 //while (1)
-//	i = 1;
-/*
-//LIM
-	printf(" %d\n", printf(_TEST6_));
-	printf(" %d\n\n",ft_printf(_TEST6_));
-	printf(" %d\n", printf(_TEST7_));
-	printf(" %d\n\n",ft_printf(_TEST7_));
+uintmax_t i = 0;
+while (i < 1000)
+{
+	ft_printf("\n");
 
-//	printf("pf= %d\n", printf(_TEST1_));
-//	printf("pf= %d\n", printf(_TEST2_));
-//	printf("pf= %d\n", printf(_TEST3_));
+ft_printf("%%\n");
+ft_printf("%d\n", 42);
+ft_printf("%d%d\n", 42, 41);
+ft_printf("%d%d%d\n", 42, 43, 44);
+ft_printf("%ld\n", 2147483647);
+ft_printf("%lld\n", 9223372036854775807);
+ft_printf("%x\n", 505);
+ft_printf("%X\n", 505);
+ft_printf("%p\n", &ft_printf);
+ft_printf("%20.15d\n", 54321);
+ft_printf("%-10d\n", 3);
+ft_printf("% d\n", 3);
+ft_printf("%+d\n", 3);
+ft_printf("%010d\n", 1);
+ft_printf("%hhd\n", 0);////
+ft_printf("%jd\n", 9223372036854775807);
+ft_printf("%zd\n", 4294967295);
+ft_printf("%\n");
+ft_printf("%U\n", 4294967295);
+ft_printf("%u\n", 4294967295);
+ft_printf("%o\n", 40);
+ft_printf("%%#08x\n", 42);
+ft_printf("%x\n", 1000);
+ft_printf("%#X\n", 1000);
+ft_printf("%s\n", NULL);
+ft_printf("%S\n", L"ݗݜशব");
+ft_printf("%50.45C\n", (char)0);
+ft_printf("%0.45C\n", (char)0);
+ft_printf("%.45C\n", (char)0);
+ft_printf("%55C\n", (char)0);
+ft_printf("%50C\n", (char)0);
+ft_printf("%50.45C\n", (char)0);
+ft_printf("%S\n", (char)0);ft_printf("%50C\n", (char)0);
+ft_printf("%S\n", (char)0);ft_printf("%.10C\n", (char)0);
+ft_printf("%S\n", (char)0);
+ft_printf("%C\n", (char)0);
+ft_printf("%S\n", (char)0);
+ft_printf("%s%s\n", "test", "test");
+ft_printf("%s%s%s\n", "test", "test", "test");
+ft_printf("%C\n", 15000);
+ft_printf("%C\n", (char)0);
 
-//	while (1)		
-//		i = 1;
-	printf("\nThis is a test%d\n", k);
-	printf("blank '+'%+++++++++d\n", k);
-	printf("Multiples - %------5s\n", a);
-	printf("This is a test%     d\n", k);
-	printf("This is a test%d\n", k);
-	printf("%+-5dend\n", i);
- 	printf("This is a test%6d\n", k);
-	printf("This is a test%-6d\n", k);
-	printf("This is a test%06d\n", k);
-	printf("test espace =>% 6d\n", k);
-	printf("This is a test%6d\n", k);
+ft_printf("\n");
+ft_printf("%%\n");
+ft_printf("%d\n", 42);
+ft_printf("%d%d\n", 42, 41);
+ft_printf("%d%d%d\n", 42, 43, 44);
+ft_printf("%ld\n", 2147483647);
+ft_printf("%lld\n", 9223372036854775807);
+ft_printf("%x\n", 505);
+ft_printf("%X\n", 505);
+ft_printf("%p\n", &ft_printf);
+ft_printf("%20.15d\n", 54321);
+ft_printf("%-10d\n", 3);
+ft_printf("% d\n", 3);
+ft_printf("%+d\n", 3);
+ft_printf("%010d\n", 1);
+ft_printf("%hhd\n", 0);
+ft_printf("%jd\n", 9223372036854775807);
+ft_printf("%zd\n", 4294967295);
+ft_printf("%\n");
+ft_printf("%U\n", 4294967295);
+ft_printf("%u\n", 4294967295);
+ft_printf("%o\n", 40);
+ft_printf("%%#08x\n", 42);
+ft_printf("%x\n", 1000);
+ft_printf("%#X\n", 1000);
+ft_printf("%s\n", NULL);
+ft_printf("%S\n", L"ݗݜशব");
+ft_printf("%s%s\n", "test", "test");
+ft_printf("%s%s%s\n", "test", "test", "test");
+ft_printf("%C\n", 15000);
+ft_printf("\n");
+ft_printf("%%\n");
+ft_printf("%d\n", 42);
+ft_printf("%d%d\n", 42, 41);
+ft_printf("%d%d%d\n", 42, 43, 44);
+ft_printf("%ld\n", 2147483647);
+ft_printf("%lld\n", 9223372036854775807);
+ft_printf("%x\n", 505);
+ft_printf("%X\n", 505);
+ft_printf("%p\n", &ft_printf);
+ft_printf("%20.15d\n", 54321);
+ft_printf("%-10d\n", 3);
+ft_printf("% d\n", 3);
+ft_printf("%+d\n", 3);
+ft_printf("%010d\n", 1);
+ft_printf("%hhd\n", 0);
+ft_printf("%jd\n", 9223372036854775807);
+ft_printf("%zd\n", 4294967295);
+ft_printf("%\n");
+ft_printf("%U\n", 4294967295);
+ft_printf("%u\n", 4294967295);
+ft_printf("%o\n", 40);
+ft_printf("%%#08x\n", 42);
+ft_printf("%x\n", 1000);
+ft_printf("%#X\n", 1000);
+ft_printf("%s\n", NULL);
+ft_printf("%S\n", L"ݗݜशব");
+ft_printf("%s%s\n", "test", "test");
+ft_printf("%s%s%s\n", "test", "test", "test");
+ft_printf("%C\n", 15000);
+i++;}
 
+/*	 printf(_TEST_);ft_putchar('\n');
+	 ft_printf(_TEST_); ft_putchar('\n');
+	ft_putchar('\n');	 
 
-//	ft_printf(_TEST_);
-//	printf(_TEST_);
-
-//printf(_TEST_);
-//ft_putchar('\n');
-printf(_TEST1_);
-	ft_printf(_TEST1_);
-
-ft_putchar('\n');
-printf(_TEST2_);
-	ft_printf(_TEST2_);
+	 ft_putchar('\n');*/
+//	printf("<---F(x) %d\n", printf(_TEST_));
+//	printf("<---USR  %d\n\n",ft_printf(_TEST_));
 	
-ft_putchar('\n');
-printf(_TEST3_);
-	ft_printf(_TEST3_);
-
-ft_putchar('\n');
-printf(_TEST4_);
-	ft_printf(_TEST4_);
+return(0);
 
 
-
-	printf("%+--5dend\n", i);
-	printf("% dend\n", i);*/
-
-	return (0);
 }
